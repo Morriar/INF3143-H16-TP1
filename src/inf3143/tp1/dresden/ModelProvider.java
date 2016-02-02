c2fa37c5f95967a72858bc862df02d5227a9887f
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-02 12:51:44 -0500

    add model provider class
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

---
+++ src/inf3143/tp1/dresden/ModelProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Alexandre Terrasa <alexandre@moz-code.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package inf3143.tp1.dresden;
+
+import inf3143.tp1.Arme;
+import inf3143.tp1.Enchantement;
+import inf3143.tp1.Equipe;
+import inf3143.tp1.Guerrier;
+import inf3143.tp1.Mage;
+import inf3143.tp1.Metier;
+import inf3143.tp1.Personnage;
+import inf3143.tp1.Pretre;
+
+// La classe ModelProvider fournit une représentation Java du modèle pour Dresden OCL.
+public class ModelProvider {
+	public Arme arme;
+	public Enchantement enchantement;
+	public Equipe equipe;
+	public Guerrier guerrier;
+	public Mage mage;
+	public Metier metier;
+	public Personnage personnage;
+	public Pretre pretre;
+}
